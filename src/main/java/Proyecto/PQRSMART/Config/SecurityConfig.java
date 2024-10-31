0a48aa197759a665cc8db5c3eb91bff6bb83163e
migueljgc <miguelgaviriam.8@gmail.com>
2024-10-30 20:41:53 -0500

    act

--- src/main/java/Proyecto/PQRSMART/Config/SecurityConfig.java
+++ src/main/java/Proyecto/PQRSMART/Config/SecurityConfig.java
@@ -45,10 +45,11 @@ import java.util.List;
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://pqrsmart.netlify.app/")); // Reemplaza con la URL de tu frontend
+        configuration.setAllowedOrigins(List.of("https://pqrsmart.netlify.app")); // Reemplaza con la URL de tu frontend
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setExposedHeaders(List.of("Authorization"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
+
 
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
